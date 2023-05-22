0b1f049d1c6ed5928a89a35c77dd9cad9e21007f
Mahesh <maheshsona28@gmail.com>
2023-05-22 11:55:38 +0530

    new form

--- index.php
+++ index.php
@@ -537,9 +537,9 @@
           <h2>Contact Us</h2>
         </div>
 
-        <div class="row">
+        <div class="">
 
-          <div class="col-lg-5 d-flex align-items-stretch" data-aos="fade-right">
+          <div class="d-flex align-items-stretch" data-aos="fade-right">
             <div class="info">
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
@@ -567,39 +567,11 @@
 
           </div>
 
-          <div id="contact-form" class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-left">
-            <form action="mail.php" method="POST" role="form" class="php-email-form">
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="name">Your Name</label>
-                  <input type="text" name="name" class="form-control" id="name" required>
-                </div>
-                <div class="form-group col-md-6 mt-3 mt-md-0">
-                  <label for="name">Your Email</label>
-                  <input type="email" class="form-control" name="email" id="email" required>
-                </div>
-              </div>
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="name">Your Contact</label>
-                  <input type="number" name="contact" class="form-control" id="contact" required>
-                </div>
-                <div class="form-group col-md-6 mt-3 mt-md-0">
-                  <label for="name">Your City</label>
-                  <input type="city" class="form-control" name="city" id="city" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <label for="name">Message</label>
-                <textarea class="form-control" name="message" rows="5" ></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit" name="send">Get Quote</button></div>
-            </form>
+          <div id="contact-form" class=" " data-aos="fade-left">
+         
+          <script src="https://sheetdb.io/s/f/1jhq5dmw3aiad.js"></script>
+          </div>
+          
           </div>
 
         </div>
